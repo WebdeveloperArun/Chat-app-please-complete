109104b6da11ef84b291715e36648fc1d621d8ee
WebDeveloperArun <forwebdeveloper0@gmail.com>
2024-08-02 12:02:53 +0530

    added auth, messages and user functionality

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,25 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js"
 import connectDB from "./db/connectDB.js";
+import messageRoutes from "./routes/messageRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+import cookieParser from "cookie-parser";
 
 process.loadEnvFile();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use("/api/auth", authRoutes);
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser())
 
-// app.get("/", (req, res) => {
+// app.get("/:id", (req, res) => {
 //   res.send("Hallo world");
 // });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
   connectDB()
